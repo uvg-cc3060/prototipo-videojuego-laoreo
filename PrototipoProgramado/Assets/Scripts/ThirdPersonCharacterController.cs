51a29653203ba8e4b99716baa9e61881feb8c9c3
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-05-14 21:56:45 -0600

    Camara y movimiento

---
+++ PrototipoProgramado/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float speed = 3;
+    //Animator anim;
+
+    private void Start()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = 5;
+        }
+        else
+        {
+            speed = 3;
+        }
+
+        //anim.SetFloat("velocity", speed);
+        Vector3 movement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+    }
+}
